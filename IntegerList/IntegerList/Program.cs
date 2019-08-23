154470b297db300f3bf9bf638e8346d2f94e4c0d
adrianamguzman <47724869+adrianamguzman@users.noreply.github.com>
2019-08-23 14:39:27 -0700

    Exceptions List

---
+++ IntegerList/IntegerList/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegerList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> {1,2,3,4,5};
+            Console.WriteLine("Choose a number:");
+
+            try
+            {
+
+                decimal numberPicked = Convert.ToDecimal(Console.ReadLine());
+
+                foreach (int number in numbers)
+                {
+
+                    decimal total = numberPicked / number;
+                    Console.WriteLine(total);
+                 
+                }
+      
+            }
+
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please type a whole number");
+                Console.ReadLine();
+                return;
+            }
+            
+
+            Console.ReadLine();
+        }
+            
+    }
+}
