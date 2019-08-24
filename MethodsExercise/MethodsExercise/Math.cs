527dfd28d2e97293af4363544227cef33fe7c5cb
adrianamguzman <47724869+adrianamguzman@users.noreply.github.com>
2019-08-24 14:03:57 -0700

    Methods&Classes Exercise

---
+++ MethodsExercise/MethodsExercise/Math.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsExercise
+{
+     class Math
+    {
+
+        public int Addme(int number)
+        {
+            int totalValue;
+            totalValue = number + 10;
+            return totalValue;
+
+        }
+        public int DivideMe(int number)
+        {
+            int totalValue;
+            totalValue = number / 10;
+            return totalValue;
+        }
+        public int MultiplyMe(int number)
+        {
+            int totalValue;
+            totalValue = number * 10;
+            return totalValue;
+        }
+    }
+}
---
+++ MethodsExercise/MethodsExercise/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MethodsExercise;
+
+namespace MethodsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MethodsExercise.Math math = new Math();
+            Console.WriteLine("Pick a number to Add:");
+            int pickedNumber = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine(math.Addme(pickedNumber));
+
+            
+            Console.WriteLine("Pick a number to divide:");
+            int pickedNumber2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(math.DivideMe(pickedNumber2));
+
+
+            Console.WriteLine("Pick a number to Multiply:");
+            int pickedNumber3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(math.MultiplyMe(pickedNumber3));
+
+
+            Console.ReadLine();
+        }
+        
+    }
+    
+
+}
