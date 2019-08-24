9c99e2409446a64943e5f91995659a6082062c02
adrianamguzman <47724869+adrianamguzman@users.noreply.github.com>
2019-08-24 16:49:27 -0700

    Passing Parameters

---
+++ MethodOverloading/MethodOverloading/Math.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    class Math
+    {
+        public int Addme(int number, int number2)
+        {
+            int totalValue;
+            totalValue = number + number2;
+            return totalValue;
+
+        }
+    }
+}
---
+++ MethodOverloading/MethodOverloading/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MethodOverloading;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MethodOverloading.Math math = new Math();
+            Console.WriteLine("Pick two numbers to Add:");
+            int pickedNumber = Convert.ToInt32(Console.ReadLine());
+            int pickedNumber2 = Convert.ToInt32(Console.ReadLine());
+
+            int total = math.Addme(pickedNumber, pickedNumber2);
+            Console.WriteLine("Yout total value is : " + total);
+            Console.ReadLine();
+        }
+    } 
+}
