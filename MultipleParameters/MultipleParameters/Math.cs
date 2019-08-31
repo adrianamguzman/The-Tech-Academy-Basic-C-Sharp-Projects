4964edfbab89143597d3b8040def827ef2a9eb27
adrianamguzman <47724869+adrianamguzman@users.noreply.github.com>
2019-08-31 16:39:09 -0700

    MultipleParameters

---
+++ MultipleParameters/MultipleParameters/Math.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleParameters
+{
+    class Math
+    {
+        public int Addme(int number, int number2=0)
+        {
+            int totalValue;
+            totalValue = number + number2 + 10;
+            return totalValue;
+
+        }
+    }
+}
---
+++ MultipleParameters/MultipleParameters/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MultipleParameters;
+
+namespace MultipleParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Math math = new Math();
+            Console.WriteLine("Pick a number to Add:");
+            int pickedNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Pick another number(optional):");
+            try
+            {
+
+                int pickedNumber2 = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+
+
+                Console.WriteLine("Your total numer is: " + math.Addme(pickedNumber));
+            }
+            Console.ReadLine();
+        }
+            
+            
+        
+    
+    }
+}
