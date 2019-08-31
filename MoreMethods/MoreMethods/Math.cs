b18aec1a086320d5d2d950ecc610f05611ecc5d3
adrianamguzman <47724869+adrianamguzman@users.noreply.github.com>
2019-08-31 15:57:25 -0700

    More Methods

---
+++ MoreMethods/MoreMethods/Math.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreMethods
+{
+    class Math
+    {
+        public int Addme(int number)
+        {
+            int totalValue;
+            totalValue = number + 10;
+            return totalValue;
+
+        }
+        public int Addme(decimal number)
+        {
+            int number1 = Convert.ToInt32(number);
+            int totalValue;
+            totalValue = number1 + 10;
+            return totalValue;
+        }
+        public int Addme (string number)
+        {
+            int number2 = Convert.ToInt32(number);
+            int totalValue;
+            totalValue = number2 + 10;
+            return totalValue;
+        }
+    }
+}
---
+++ MoreMethods/MoreMethods/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MoreMethods;
+
+namespace MoreMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+                MoreMethods.Math math = new Math();
+
+            try
+            {
+                Console.WriteLine("Pick an integer to Add:");
+
+
+                int pickedNumber = Convert.ToInt32(Console.ReadLine());
+
+
+             
+                    Console.WriteLine(math.Addme(pickedNumber));
+
+
+
+                    Console.WriteLine("Pick a decimal to Add:");
+                    decimal pickedNumber2 = Convert.ToDecimal(Console.ReadLine());
+
+                    Console.WriteLine(math.Addme(pickedNumber2));
+
+               
+            }
+            catch
+            {
+               
+                string pickedNumber3 = Console.ReadLine();
+
+                Console.WriteLine(math.Addme(pickedNumber3));
+            }
+
+                Console.ReadLine();
+
+
+            
+        }
+    }
+}
