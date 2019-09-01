bf9897ed1e69f9cb90414f284ce9f60e8111e792
adrianamguzman <47724869+adrianamguzman@users.noreply.github.com>
2019-08-31 18:20:56 -0700

    static non static exercise

---
+++ VoidMethodExercise/VoidMethodExercise/Math.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodExercise
+{
+    class Math
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    
+        public void doMath(int number)
+        {
+            int totalValue;
+            totalValue = number / 2;
+            Console.WriteLine(number + "/2 =" + totalValue);
+
+        }
+
+        public static string FullName(string firstName, string lastName)
+
+            {
+            string totalName = "that is your answer " + firstName + " " + lastName;
+            return totalName ;
+
+            }   
+        
+    }
+}
---
+++ VoidMethodExercise/VoidMethodExercise/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VoidMethodExercise;
+
+namespace VoidMethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+
+            Console.WriteLine("What is your first name?");
+
+            math.firstName = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            math.lastName = Console.ReadLine();
+            
+            Console.WriteLine("Pick a number to Divide:");
+            int pickedNumber = Convert.ToInt32(Console.ReadLine());
+
+           
+            math.doMath(pickedNumber);
+
+
+            Console.WriteLine(Math.FullName(math.firstName,math.lastName));
+            
+            Console.ReadLine();
+        }
+
+
+    
+    }
+}
