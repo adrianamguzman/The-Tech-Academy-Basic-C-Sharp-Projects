fe0b41a9797a708fbec83d47d9e2221944431609
adrianamguzman <47724869+adrianamguzman@users.noreply.github.com>
2019-09-05 17:16:16 -0700

    Struct Exercise

---
+++ Struct Exercise/Struct Exercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 5;
+
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
