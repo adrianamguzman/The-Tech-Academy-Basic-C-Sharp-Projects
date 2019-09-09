167ec1477e0ca457fe8a10ccf5b316f78d60469e
adrianamguzman <47724869+adrianamguzman@users.noreply.github.com>
2019-09-08 20:48:27 -0700

    logging

---
+++ Logging/Logging/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Logging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number of your choice: ");
+            string number = Console.ReadLine();
+
+            File.WriteAllText(@"C:\Users\Adriana\source\logPractice.txt", number);
+
+            string text = File.ReadAllText(@"C:\Users\Adriana\source\logPractice.txt");
+
+            System.Console.WriteLine("Contents of logPractice.txt = {0}", text);
+
+            Console.ReadLine();
+
+        }
+    }
+}
