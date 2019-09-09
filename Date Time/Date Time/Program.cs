e0fa07d9ba3747c0f692b11a1eb08c75ac4cd9b5
adrianamguzman <47724869+adrianamguzman@users.noreply.github.com>
2019-09-09 12:28:46 -0700

    DateTime

---
+++ Date Time/Date Time/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Date_Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+           
+
+            Console.WriteLine("Enter a number of hours: ");
+            int hours = Convert.ToInt32(Console.ReadLine());
+
+            DateTime futureTime = DateTime.Now.AddHours(hours);
+            Console.WriteLine(futureTime);
+            Console.ReadLine();
+
+
+        }
+    }
+}
