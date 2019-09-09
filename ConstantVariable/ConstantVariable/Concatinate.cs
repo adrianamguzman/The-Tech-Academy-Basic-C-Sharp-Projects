8df1659301a8b98d75bc43f361246092adfffde4
adrianamguzman <47724869+adrianamguzman@users.noreply.github.com>
2019-09-09 14:11:08 -0700

    Constant Var

---
+++ ConstantVariable/ConstantVariable/Concatinate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstantVariable
+{
+    public class Concatinate
+
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public Concatinate(string name) : this(name, string.Empty)
+        {
+        }
+
+       
+
+        public Concatinate(string name, string name2)
+        {
+            this.firstName = name;
+           
+            this.lastName = name2;
+        }
+
+
+    }
+}
---
+++ ConstantVariable/ConstantVariable/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstantVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string type = "Student";
+            Console.WriteLine("What is your last name?");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("What is your first name?");
+            string firstName = Console.ReadLine();
+
+            var fullName = new Concatinate(lastName,firstName);
+            Console.WriteLine("{0} : {1} {2}", type, firstName, lastName);
+            Console.ReadLine();
+
+         
+
+        }
+    }
+}
+
+
+// ** yes i know no real concatinating was done in this code hahah
